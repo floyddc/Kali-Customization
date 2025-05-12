1499483255048564781fd7fd4d86448864b99fa1
floyddc <diegocoli68@gmail.com>
2025-05-12 07:54:34 +0200

    refactor

--- installer.sh
+++ installer.sh
@@ -21,45 +21,45 @@ echo "Nerd fonts installed."
 
 # lsd config
 mkdir -p /etc/lsd
-cp "$current_path/lsd_config.yaml" /etc/lsd/config.yaml
-cp "$current_path/lsd_colors.yaml" /etc/lsd/colors.yaml
+cp "$current_path/lsd/lsd_config.yaml" /etc/lsd/config.yaml
+cp "$current_path/lsd/lsd_colors.yaml" /etc/lsd/colors.yaml
 echo "lsd configured."
 
 # Polybar config
 mkdir -p /etc/polybar
-cp "$current_path/polybar_config.ini" /etc/polybar/config.ini
+cp "$current_path/polybar/polybar_config.ini" /etc/polybar/config.ini
 mkdir -p /etc/polybar/scripts
-cp "$current_path/target_setter.sh" /etc/polybar/scripts/target_setter.sh
+cp "$current_path/polybar/target_setter.sh" /etc/polybar/scripts/target_setter.sh
 chmod 777 /etc/polybar/scripts/target_setter.sh
-cp "$current_path/polybar.desktop" /etc/xdg/autostart/polybar.desktop
+cp "$current_path/polybar/polybar.desktop" /etc/xdg/autostart/polybar.desktop
 echo "Polybar configured."
 
 # vivid config
 mkdir -p /etc/vivid
 mkdir -p /etc/vivid/themes
-cp "$current_path/vivid_theme.yml" /etc/vivid/themes/theme.yml
+cp "$current_path/vivid/vivid_theme.yml" /etc/vivid/themes/theme.yml
 echo "vivid configured."
 
 # Rofi config
 mkdir -p /etc/rofi
 mkdir -p /usr/share/rofi
 mkdir -p /usr/share/rofi/themes
-cp "$current_path/rofi_theme.rasi" /usr/share/rofi/themes/theme.rasi
+cp "$current_path/rofi/rofi_theme.rasi" /usr/share/rofi/themes/theme.rasi
 echo "Rofi configured."
 
 # Remove terminal border
 mkdir -p /etc/skel/.config/devilspie2
-cp "$current_path/qterminal.lua" /etc/skel/.config/devilspie2/qterminal.lua
-cp "$current_path/qterminal.conf" /etc/xdg/qterminal.org/qterminal.conf
-cp "$current_path/devilspie2.desktop" /etc/xdg/autostart/devilspie2.desktop
+cp "$current_path/qterminal-borders/qterminal.lua" /etc/skel/.config/devilspie2/qterminal.lua
+cp "$current_path/qterminal-borders/qterminal.conf" /etc/xdg/qterminal.org/qterminal.conf
+cp "$current_path/qterminal-borders/devilspie2.desktop" /etc/xdg/autostart/devilspie2.desktop
 echo "Terminal border removed."
 
 # ohmyzsh / p10k config
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 mv /root/.oh-my-zsh /etc/oh-my-zsh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /etc/oh-my-zsh/themes/powerlevel10k 
-exit
-cp "$current_path/globalZshrc" /etc/zsh/zshrc
+cp "$current_path/ohmyzsh-p10k/globalZshrc" /etc/zsh/zshrc
+exec bash -c "exit"
 
 echo "source /etc/zsh/zshrc" | tee /etc/skel/.zshrc > /dev/null
 for user in $(cut -f1 -d: /etc/passwd); do
@@ -77,7 +77,7 @@ else
     echo "Line already exists in /root/.zshrc"
 fi
 
-cp "$current_path/p10k" /etc
+cp "$current_path/ohmyzsh-p10k/p10k" /etc
 
 for user in $(cut -f1 -d: /etc/passwd); do
     if [ -d /home/$user ]; then
@@ -99,5 +99,5 @@ source /etc/zsh/zshrc
 [[ ! -f /etc/.p10k.zsh ]] || source /etc/.p10k.zsh
 EOF
 
-cp "$current_path/rootZshrc" /root/.zshrc
+cp "$current_path/ohmyzsh-p10k/rootZshrc" /root/.zshrc
 echo "Customization completed."
